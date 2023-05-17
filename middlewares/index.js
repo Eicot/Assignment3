4924e34057d1f11b763d08778c10c7f35d22d165
Eicot <zha232zha@gmail.com>
2023-05-17 08:58:49 +0000

    Parallel Lab 17

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const checkIfAuthenticated = (req, res, next) => {
     if (req.session.user) {
         next()
@@ -6,6 +8,27 @@ const checkIfAuthenticated = (req, res, next) => {
         res.redirect('/users/login');
     }
 }
+
+
+
+const checkIfAuthenticatedJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 module.exports = {
     checkIfAuthenticated
